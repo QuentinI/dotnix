25e399d8d3bdbfa42518eea1b6e4c47270398c28
Quentin <Quentin Inkling>
2020-06-30 01:34:28 +0300

    Utils: add quiet build option

--- utils.sh
+++ utils.sh
@@ -4,6 +4,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+QUIET_BUILD=false
+
+# Nicholas Sushkin
+# https://stackoverflow.com/questions/1527049/how-can-i-join-elements-of-an-array-in-bash
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
 function usage() {
 cat <<EOF
 Usage:
@@ -15,7 +21,21 @@ EOF
 }
 
 function trace() {
-    echo -e "\e[1;34m> $*\e[0m" >&2; "$@"
+    if $QUIET_BUILD
+    then
+        log="$(mktemp)"
+        echo -e "\e[1;34m> $@ \e[0m [logging to $log]" >&2
+        "$@" > $log 2>&1 && stat=$? || stat=$?
+        if [ $stat -ne 0 ]
+        then
+            echo -e "\e[1;31m> FAILED <\e[0m"
+            head -n 50 $log
+            exit $stat
+        fi
+    else
+        echo -e "\e[1;34m> $@ \e[0m" >&2
+        "$@"
+    fi
 }
 
 function build() {
@@ -23,7 +43,7 @@ function build() {
     home_manager_path="$(nix eval --raw "(import ${CFG_PATH}/nix/sources.nix).home-manager.outPath")"
     export NIX_PATH=nixpkgs="${nixpkgs_path}":home-manager="${home_manager_path}":nixpkgs-overlays="$(realpath $CFG_PATH)/overlays/"
     tmp=$(mktemp -u)
-    trace nix build --no-link -f "${CFG_PATH}" -o "${tmp}/result" --keep-going --show-trace "$*" >&2
+    trace nix build --no-link -f "${CFG_PATH}" -o "${tmp}/result" --keep-going --show-trace "$@" >&2
     drv="$(readlink "${tmp}/result")"
     echo "${drv}"
 }
@@ -68,26 +88,45 @@ CFG_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mode="$1"
 shift
 
+while ! [ -z ${1+x} ]
+do
+    case "$1" in
+        "--")
+            shift
+            break
+            ;;
+        "-Q" | "--quiet")
+            echo "Building quietly"
+            QUIET_BUILD=true
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
 case "$mode" in
     "build")
-        build "$*"
+        build "$@"
         ;;
     "switch")
-        switch "$*"
+        switch "$@"
         ;;
     "check")
-        check "$*"
+        check "$@"
         ;;
     "update")
-        update "$*"
+        update "$@"
         ;;
     "upgrade")
-        upgrade "$*"
+        upgrade "$@"
         ;;
     "format")
-        format "$*"
+        format "$@"
         ;;
     "vm")
-        vm "$*"
+        vm "$@"
         ;;
 esac

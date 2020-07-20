a9d193e35bc2a7f54ffd4b07de6dc2cf2982b9af
Quentin <Quentin Inkling>
2020-07-20 14:18:26 +0300

    FF tweaks

--- home/programs/firefox/default.nix
+++ home/programs/firefox/default.nix
@@ -61,12 +61,6 @@ in {
           # JS leaks locale
           "javascript.use_us_english_locale" = true;
 
-          # Set proxy
-          "network.proxy.socks" = "127.0.0.1";
-          "network.proxy.socks_port" = 1080;
-          "network.proxy.socks_remote_dns" = true;
-          "network.proxy.type" = 1;
-
           # Disable telemetry and telemetry and telemetry and...
           # How many more of this shit is there?
           "toolkit.telemetry.unified" = false;

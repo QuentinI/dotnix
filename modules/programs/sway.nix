d8843bef4588321bd57ff885556b9d5c573c9015
Quentin <Quentin Inkling>
2021-02-10 12:02:14 +0300

    Set environment variables correctly for sway session

--- modules/programs/sway.nix
+++ modules/programs/sway.nix
@@ -3,13 +3,6 @@
 {
   environment.systemPackages = [ pkgs.qt5.qtwayland pkgs.sway ];
 
-  environment.variables = {
-    SDL_VIDEODRIVER = "wayland";
-    QT_QPA_PLATFORM = "wayland";
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-    MOZ_ENABLE_WAYLAND = "1";
-    _JAVA_AWT_WM_NONREPARENTING="1";
-  };
   security.pam.services.swaylock = { };
 
   services.xserver = {
--- users/modules/programs/sway/default.nix
+++ users/modules/programs/sway/default.nix
@@ -132,7 +132,14 @@ in rec {
   ];
 
   # Get rid of Qt filepickers, etc
-  home.sessionVariables = { "XDG_CURRENT_DESKTOP" = "XFCE"; };
+  home.sessionVariables = {
+    SDL_VIDEODRIVER = "wayland";
+    QT_QPA_PLATFORM = "wayland";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    _JAVA_AWT_WM_NONREPARENTING="1";
+    XDG_CURRENT_DESKTOP = "XFCE";
+  };
 
   # Waybar works with libappindicator tray icons only
   xsession.preferStatusNotifierItems = true;
